e6a6b872cb879e52bd6e9bb37d9d9421a695e971
abdulkhaliq784 <khaliqsaad18@gmail.com>
2024-12-18 00:31:11 +0530

    Add files via upload

---
+++ LaunchBrowser.java
@@ -0,0 +1,51 @@
+package week3day4;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class LaunchBrowser {
+	public static void main(String[] args) throws InterruptedException {
+		
+	 ChromeDriver driver= new ChromeDriver();
+	
+
+	 
+		
+	 driver.get("http://leaftaps.com/opentaps/control/main");
+	 driver.manage().window().maximize();
+	 
+	 driver.findElement(By.id("username")).sendKeys("demosalesmanager");
+	 driver.findElement(By.name("PASSWORD")).sendKeys("crmsfa");
+	 driver.findElement(By.className("decorativeSubmit")).click();
+	 driver.findElement(By.partialLinkText("CRM/SFA")).click();
+	 driver.findElement(By.linkText("Leads")).click();
+	 String title1 = driver.getTitle();
+	 System.out.println("title1");
+	 driver.findElement(By.linkText("Create Lead")).click();
+	 String title = driver.getTitle();
+	 System.out.println("title");
+	 String currentUrl = driver.getCurrentUrl();
+	 System.out.println("currentUrl");
+	// driver.findElement(By.tagName("a")).click();
+	List <WebElement> var = driver.findElements(By.tagName("a"));
+	     for(int i = 0; i<var.size();i++) {
+	    	 String text = var.get(i).getText();
+				System.out.println(text);
+	     }
+	     driver.findElement(By.id("createLeadForm_companyName")).sendKeys("TestLeaf");
+	     driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Abdul");
+	     driver.findElement(By.id("createLeadForm_lastName")).sendKeys("Khaliq");
+	     WebElement source = 
+	     driver.findElement(By.id("createLeadForm_dataSourceId"));
+	   Select obj = new Select (source);
+	   obj.selectByIndex(2);
+	   obj.selectByVisibleText("Employee");
+	   Thread.sleep(3000);
+	   obj.selectByValue("LEAD_DIRECTMAIL");
+	   obj.deselectByValue("LEAD_DIRECTMAIL");
+	}
+}
